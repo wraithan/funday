b7d9272d03b1063a70bdcdb69694c50bb8bc16e2
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2012-09-13 06:20:53 -0700

    switch to gunicorn and add procfile

--- anyday/settings.py
+++ anyday/settings.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'anyday.core',
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
